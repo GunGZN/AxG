f3d0c3b3d761ae0f87e5590d5e8f20c13b800e65
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-10-03 06:12:14 +0700

    Update menu2.sh

--- menu2.sh
+++ menu2.sh
@@ -2,22 +2,22 @@ kguza="https://kguza.net/scrip/u-d/openvpn"
 # download script
 cd /usr/bin
 wget -q -O m2 "https://raw.githubusercontent.com/GunGZN/AxG/main/menu.sh"
-wget -q -O 01 "$kguza/menu/adduser.sh"
-wget -q -O 02 "$kguza/menu/testuser.sh"
-wget -q -O 03 "$kguza/menu/ChangPass.sh"
-wget -q -O 04 "$kguza/menu/changExp.sh"
-wget -q -O 05 "$kguza/menu/delet.sh"
-wget -q -O 06 "$kguza/menu/deletuserxp.sh"
-wget -q -O 07 "$kguza/menu/viewuser.sh"
-wget -q -O 08 "$kguza/menu/restart.sh"
-wget -q -O 09 "$kguza/menu/speedtest.py"
-wget -q -O 10 "$kguza/menu/vnstat.sh"
+wget -q -O a1 "$kguza/menu/adduser.sh"
+wget -q -O a2 "$kguza/menu/testuser.sh"
+wget -q -O a3 "$kguza/menu/ChangPass.sh"
+wget -q -O a4 "$kguza/menu/changExp.sh"
+wget -q -O a5 "$kguza/menu/delet.sh"
+wget -q -O a6 "$kguza/menu/deletuserxp.sh"
+wget -q -O a7 "$kguza/menu/viewuser.sh"
+wget -q -O a8 "$kguza/menu/restart.sh"
+wget -q -O a9 "$kguza/menu/speedtest.py"
+wget -q -O a10 "$kguza/menu/vnstat.sh"
 wget -q -O k "$kguza/menu/b-user.sh"
 wget -q -O 31 "$kguza/menu/max.sh"
-wget -q -O 12 "$kguza/menu/lock.sh"
-wget -q -O 13 "$kguza/menu/unlock.sh"
-wget -q -O 14 "$kguza/menu/httpcredit.sh"
-wget -q -O 15 "$kguza/menu/aboutscrip.sh"
+wget -q -O a12 "$kguza/menu/lock.sh"
+wget -q -O a13 "$kguza/menu/unlock.sh"
+wget -q -O a14 "$kguza/menu/httpcredit.sh"
+wget -q -O a15 "$kguza/menu/aboutscrip.sh"
 wget -q -O 16 "$kguza/menu/TimeReboot.sh"
 wget -q -O 17 "$kguza/menu/backup.sh"
 wget -q -O 18 "$kguza/menu/setspeed.sh"
@@ -38,28 +38,28 @@ wget -q -O panel "$kguza/menu/Ocs-panel.sh"
 wget -q -O 30 "$kguza/menu/squid.sh"
 wget -q -O 443 "$kguza/menu/443.sh"
 wget -q -O 32 "$kguza/menu/m443.sh"
-wget -q -O 11 "$kguza/menu/port.sh"
+wget -q -O a11 "$kguza/menu/port.sh"
 wget -q -O squid3.3 "$kguza/menu/squid3.3.sh"
 
 echo "30 3 * * 0 root /sbin/reboot" > /etc/cron.d/reboot
 
 chmod +x m2
-chmod +x 01
-chmod +x 02
+chmod +x a1
+chmod +x a2
 chmod +x 03
-chmod +x 04
-chmod +x 05
-chmod +x 06
-chmod +x 07
-chmod +x 08
-chmod +x 09
-chmod +x 10
+chmod +x a4
+chmod +x a5
+chmod +x a6
+chmod +x a7
+chmod +x a8
+chmod +x a9
+chmod +x a10
 chmod +x k
 chmod +x 31
-chmod +x 12
-chmod +x 13
-chmod +x 14
-chmod +x 15
+chmod +x a12
+chmod +x a13
+chmod +x a14
+chmod +x a15
 chmod +x 16
 chmod +x 17
 chmod +x 18
@@ -80,5 +80,5 @@ chmod +x panel
 chmod +x 30
 chmod +x 443
 chmod +x 32
-chmod +x 11
+chmod +x a11
 chmod +x squid3.3
